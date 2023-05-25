bd54a7c6273eba437db8b3a4ea2b9026256beec4
erik47111 <erik4711111@gmail.com>
2023-05-25 22:55:42 +0200

    Main,mainapplication megírása

---
+++ src/main/java/Kinggames/game/gameui/contoller/Main.java
@@ -0,0 +1,8 @@
+package Kinggames.game.gameui.contoller;
+import Kinggames.game.gameui.contoller.GameApplication;
+import javafx.application.Application;
+public class Main {
+   public static void main(String[]args){
+       Application.launch(GameApplication.class,args);
+   }
+}
